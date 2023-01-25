3418e77f5c5e6e304567accd4234278eb5dfd765
liv3rise <liv3rise@gmail.com>
2023-01-25 10:10:53 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,12 @@ import imagemin from 'unplugin-imagemin/vite';
 export default defineConfig({
   plugins: [ 
     imagemin({
-      // Default mode squoosh. support squoosh and sharp
+      beforeBundle: false,
       mode: 'sharp',
-      // Default configuration options for compressing different pictures
-      compress: {
-        jpg: {
-          quality: 0,
-        },
-        jpeg: {
-          quality: 70,
-        },
-        png: {
-          quality: 70,
-        },
-        webp: {
-          quality: 70,
-        },
-      },
-      // The type of picture converted after the build
       conversion: [
         { from: 'png', to: 'webp' },
         { from: 'jpeg', to: 'webp' },
+        { from: 'jpg', to: 'webp' },
       ]
     })
   ]
